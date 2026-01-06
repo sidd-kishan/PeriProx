06c959aae17715b7f31c15c8d25adf822a7ef93e
Aayush Deo <aayush.deo1994@gmail.com>
2026-01-06 23:09:02 +0530

    Add files via upload

---
+++ start_minimal_x86_64.bat
@@ -0,0 +1,10 @@
+qemu-img create -f qcow2 alpine_disk.qcow2 8G
+
+qemu-system-x86_64.exe -accel tcg -m 128M -kernel .\boot\vmlinuz-virt -initrd .\boot\initramfs-virt -append "console=ttyS0 alpine_serial=ttyS0 alpine_dev=cdrom" -drive file=alpine-virt-3.23.2-x86_64.iso,media=cdrom -drive file=alpine_disk.qcow2,if=none,format=qcow2,id=hd0 -device virtio-blk-pci,drive=hd0 -nographic
+
+ip addr add 10.0.2.15/24 dev eth0
+ip link set eth0 up
+ip route add default via 10.0.2.2
+export http_proxy=http://192.168.4.5:8080
+
+qemu-system-x86_64.exe  -m 128M  -kernel .\boot\vmlinuz-virt  -initrd .\boot\initramfs-virt  -append "console=ttyS0 root=/dev/vda3 rootwait rw alpine_serial=ttyS0 rootfstype=ext4"  -drive file=alpine_disk.qcow2,if=none,format=qcow2,id=hd0 -device virtio-blk-pci,drive=hd0 -nographic
