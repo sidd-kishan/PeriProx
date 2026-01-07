4513123a49a75477fdef8cc5687598d09444b282
Aayush Deo <aayush.deo1994@gmail.com>
2026-01-07 22:19:15 +0530

    Enhance startup script with network and proxy setup
    Added network setup and proxy configuration commands.

--- start_minimal_x86_64.bat
+++ start_minimal_x86_64.bat
@@ -2,9 +2,16 @@ qemu-img create -f qcow2 alpine_disk.qcow2 8G
 
 qemu-system-x86_64.exe -accel tcg -m 128M -kernel .\boot\vmlinuz-virt -initrd .\boot\initramfs-virt -append "console=ttyS0 alpine_serial=ttyS0 alpine_dev=cdrom" -drive file=alpine-virt-3.23.2-x86_64.iso,media=cdrom -drive file=alpine_disk.qcow2,if=none,format=qcow2,id=hd0 -device virtio-blk-pci,drive=hd0 -nographic
 
-ip addr add 10.0.2.15/24 dev eth0
+setup-interface
+setup-proxy
+setup-apkrepos -f
+
+
+
+qemu-system-x86_64.exe  -m 128M  -kernel .\boot\vmlinuz-virt  -initrd .\boot\initramfs-virt  -append "console=ttyS0 root=/dev/vda3 rootwait rw alpine_serial=ttyS0 rootfstype=ext4  alpine_dev=cdrom"  -drive file=alpine_disk.qcow2,if=none,format=qcow2,id=hd0 -drive file=alpine-virt-3.23.2-x86_64.iso,media=cdrom -device virtio-blk-pci,drive=hd0 -netdev user,id=n1 -device virtio-net-pci,netdev=n1 -nographic
+
 ip link set eth0 up
+ip addr add 10.0.2.15/24 dev eth0
 ip route add default via 10.0.2.2
 export http_proxy=http://192.168.4.5:8080
-
-qemu-system-x86_64.exe  -m 128M  -kernel .\boot\vmlinuz-virt  -initrd .\boot\initramfs-virt  -append "console=ttyS0 root=/dev/vda3 rootwait rw alpine_serial=ttyS0 rootfstype=ext4"  -drive file=alpine_disk.qcow2,if=none,format=qcow2,id=hd0 -device virtio-blk-pci,drive=hd0 -nographic
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
